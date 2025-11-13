365c78c63a4e7eb6f507e39887b83e6978f28ca7
andrearomanato06-arch <andrearomanato06@gmail.com>
2025-11-13 14:36:52 +0100

    added LastName and FirstName to User

--- src/main/java/it/eforhum/authModule/entities/User.java
+++ src/main/java/it/eforhum/authModule/entities/User.java
@@ -24,6 +24,12 @@ public class User{
     @Column(name = "PasswordHash" ,nullable = false)
     private String PasswordHash;
 
+    @Column(name="FirstName")
+    private String FirstName;
+
+    @Column(name = "LastName")
+    private String LastName;
+
     @Column(name = "Active")
     private boolean Active;
 
@@ -35,10 +41,12 @@ public class User{
 
     public User(){}
 
-    public User(int id,String email,String passwordHash, boolean active,LocalDateTime creationDate,LocalDateTime lastAccessDate){
+    public User(int id,String email,String passwordHash, String FirstName,String LastName,boolean active,LocalDateTime creationDate,LocalDateTime lastAccessDate){
         this.UserId = id;
         this.Email = email;
         this.PasswordHash = passwordHash;
+        this.FirstName = FirstName;
+        this.LastName = LastName;
         this.Active = active;
         this.CreationDate = creationDate;
         this.LastAccessDate = lastAccessDate;
@@ -87,6 +95,22 @@ public class User{
 
     public void setLastAccessDate(LocalDateTime LastAccessDate) {
         this.LastAccessDate = LastAccessDate;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
     }
 
 }
